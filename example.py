5287d1371202cab342814230a5db7bea45e49136
Andy Durden <andys.durden@gmail.com>
2022-05-28 04:28:58 -0400

    Hessian and Gradient calls, output cleaned up

--- example.py
+++ example.py
@@ -7,10 +7,10 @@ np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
 TERACHEM = "/home/adurden/terachem/build/bin/" # terachem executable stored here
 # make sure you include temppath and tempname in your job template!
 #  those keywords are search and replaced
-job_template_contents = "#!/bin/bash\n\
-                         source /home/adurden/.bashrc\n\
-                         cd temppath\n\
-                        "+TERACHEM+"terachem tempname.in > tempname.out\n"
+job_template_contents = "#!/bin/bash\n"+\
+                        "source /home/adurden/.bashrc\n"+\
+                        "cd temppath\n"+\
+                        TERACHEM+"terachem tempname.in > tempname.out\n"
 
 JOB_TEMPLATE = "/home/adurden/tccontroller/templates/template.job"
 f = open(JOB_TEMPLATE,'w')
@@ -22,9 +22,9 @@ nfields = 1                # number of distinct fields (generally for multichrom
 nstep = 12000               # number of timesteps
 tdci_simulation_time = 12   # in femtoseconds
 krylov_end = True           # Generate approximate eigenstates at end of calculation?
-krylov_end_n = 64           # Number of steps to save wfn on to generate approx eigenstates with.
+krylov_end_n = 6           # Number of steps to save wfn on to generate approx eigenstates with.
                             #   There will be 2*krylov_end_n approximate eigenstates returned.
-krylov_end_interval = 80     # Number of steps between saved steps.
+krylov_end_interval = 20     # Number of steps between saved steps.
 tdci_options = {
   "gpus"                 : "1 0",
   "timings"              : "yes",
@@ -43,8 +43,8 @@ tdci_options = {
   "tdci_eshift"          : "gs",
   "tdci_stepprint"       : "1",
   "tdci_nfields"         : str(nfields),
-  "tdci_laser_freq"      : "3.018721363175e+15", # "2.5311296E+15",  
-  "tdci_photoneng"       : "0.45879422",         # "0.38467766",
+  "tdci_laser_freq"      : '3.440586079783e+15',  
+  "tdci_photoneng"       : "0.52291047",
   "tdci_fstrength"       : "1.0E+16",   # TODO: replace field generation params with file readin
   "tdci_fdirection"      : "x",
   "tdci_ftype"           : "cw",
@@ -57,12 +57,13 @@ tdci_options = {
   "dcimaxiter"           : "300",
   "dciprintinfo"         : "yes",
   "dcipreconditioner"    : "orbenergy",
-  "closed"               : "5",
-  "active"               : "6",
+  "closed"               : "6",
+  "active"               : "4",
   "cassinglets"          : "3",
   "castriplets"          : "0",
   "cascharges"           : "yes",
   "cas_ntos"             : "yes",
+  "mincheck"             : "false",
   "tdci_gradient"        : "yes",  # <-- don't change this
   "tdci_fieldfile0"      : "field0.bin",
 
@@ -83,8 +84,8 @@ if krylov_end:
   tdci_options["tdci_krylov_init"] = "yes"
   tdci_options["tdci_krylovmo_readfile"] = "cn_krylov_init.bin"
 
-TDCI_TEMPLATE = "/home/adurden/tccontroller/templates/tdci.in"
-tccontroller.dict_to_file(tdci_options, TDCI_TEMPLATE)
+#TDCI_TEMPLATE = "/home/adurden/tccontroller/templates/tdci.in"
+#tccontroller.dict_to_file(tdci_options, TDCI_TEMPLATE)
 
 # Define the external field
 # ======================================
@@ -119,12 +120,46 @@ FIELD_INFO = { "tdci_simulation_time": tdci_simulation_time,
 JOBDIR = "/home/adurden/jobs/testing/"
 SCHEDULER = False # Not implemented, but this will hook into sbatch/squeue
 
-tc = tccontroller.tccontroller(JOBDIR, JOB_TEMPLATE, TDCI_TEMPLATE, FIELD_INFO, SCHEDULER)
+tc = tccontroller.tccontroller(JOBDIR, JOB_TEMPLATE, tdci_options, FIELD_INFO, SCHEDULER)
 print("initialized tccontroller\n")
+
+initial_xyzfile = "/home/adurden/jobs/testing/ethylene.xyz"
+
+"""
+  Dictionary keys in grad output:
+           "eng"               - float, Energy of current wfn
+           "grad"              - 2d array, Natoms x 3 dimensions.
+
+  INPUT:
+            xyz                - string, path of xyz file.
+            ReCn (optional)    - Real component of CI vector. If none, ground state is used. 
+            ImCn (optional)    - Imaginary component of CI vector.
+"""
+grad_data = tc.grad(initial_xyzfile)
+print("Gradient output")
+print(grad_data)
+print("Gradient output end.")
 
 
 """
-  Dictionary keys in TCdata:
+  Dictionary keys in nextstep output:
+           "hessian"           - 2d array, (3*Natoms) x (3*Natoms)
+           "dipolederiv"       - Dipole Derivatives, 1d array 3*3*Natoms
+
+  INPUT:
+            xyz                - string, path of xyz file.
+            temp               - Temperature (Kelvin) of frequency calculation 
+"""
+# INPUTS: XYZ, Temperature (Kelvin)
+hess_data = tc.hessian(initial_xyzfile, 300)
+print("Hessian output:")
+print(hess_data)
+print("Hessian output end.")
+
+
+
+"""
+  Dictionary keys in nextstep output:
            "recn"              - 1d array, number of determinants (ndets)
            "imcn"              - 1d array, ndets
            "eng"               - float, Energy of current wfn
@@ -135,14 +170,20 @@ print("initialized tccontroller\n")
            "krylov_energies"   - 1d array of energies of each approx eigenstate
            "krylov_gradients"  - 3d array of approx eigenstate gradients, Napprox x Natoms x 3dim
 
+  INPUT:
+            xyz                - string, path of xyz file.
+            ReCn (optional)    - Real component of CI vector. If none, ground state is used. 
+            ImCn (optional)    - Imaginary component of CI vector.
 """
-
 # Doing the next TDCI step is as simple as calling tc.nextstep and feeding it the newest coordinates
-initial_xyzfile = "/home/adurden/jobs/testing/ethylene.xyz"
+
 TCdata = tc.nextstep(initial_xyzfile)
+print(TCdata.keys())
+print("Nextstep output:")
 print(TCdata)
 
 # Let's do 10 steps, just feeding TDCI the same geometry. 
+#"""
 for i in range(0, 10):
   print("step done!")
   # ReCn/ImCn = None (or unspecified) will simply use the end ReCn from the previous step.
@@ -152,7 +193,7 @@ for i in range(0, 10):
   ImCn = TCdata["imcn_krylov"]
   TCdata = tc.nextstep(initial_xyzfile, ReCn, ImCn)
   print(TCdata)
-
+#"""
 print("all done!")
 
 
